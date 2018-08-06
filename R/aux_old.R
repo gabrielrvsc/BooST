21ceb1eeb2ad64c2f8d7523195e1398aed77d21d
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2018-08-06 20:09:56 -0300

    +

--- R/aux_old.R
+++ R/aux_old.R
@@ -1,7 +1,8 @@
-
-grow0 = function(x,y,p,N,gamma,node_obs){
+grow_tree=function(x, y, p, d_max, gamma, node_obs){
+  bf=0
   variables = sample(ncol(x), round(p*ncol(x)))#sort(sample(1:ncol(x), max(round(p*ncol(x)),2)  ))
   gammai=gamma[sample(1:length(gamma),1)]
+  N=length(y)
   fit=list()
   for(i in 1:length(variables)){
     xtest=x[,variables[i]]
@@ -30,87 +31,71 @@ grow0 = function(x,y,p,N,gamma,node_obs){
 
   Pmat=1/(1+exp(-node["gamma"]*(x[,variables[best]]-node["c0"])))
   Pmat=cbind(Pmat,1-Pmat)
-  return(list(tree = tree, Pmat=Pmat))
-}
-
-grow = function(x,y,p,N,gamma,node_obs,tree,Pmat,d_max,iter,bf){
-  gammai=gamma[sample(1:length(gamma),1)]
-  terminal=which(tree$terminal=="yes")
-  variables=sample(ncol(x), round(p*ncol(x)))#sort(sample(1:ncol(x), max(round(p*ncol(x)),2)))
-  test=expand.grid(variables,terminal)
-  colnames(test)=c("variable","terminal")
-
-  t1=Sys.time()
-  fit=list()
-  for(i in 1:nrow(test)){
-    xt=x[,test[i,"variable"]]
-    #xtest=stats::runif(10,min(xt)-0.1*stats::sd(xt),max(xt)+0.1*stats::sd(xt))
-    xtest=sample(xt,min(20,N),prob = Pmat[,test$terminal[i]]+0.01)
-
-    gammascale=max(stats::sd(xt),0.1)
-    middlenodes=which(is.na(colSums(Pmat)))
-
-    fit[[i]]=c(val=Inf)
-    ssr=sapply(xtest,node_var_test,x=x[,test[i,"variable"]],y=y,gamma=gammai/gammascale,Pmat=Pmat,
-               terminal=test$terminal[i],middlenodes=middlenodes,deep=tree$deep[test$terminal[i]]+1,node_obs=node_obs)
-    ssr=t(ssr)
-    ssr[is.nan(ssr)]=Inf
-    best=which.min(ssr[,1])
-    res=c(xtest[best],gammai/gammascale,ssr[best,])
-    names(res)=c("c0","gamma","val","b0","b1")
-    fit[[i]]=res
-
-  }
-  Sys.time()-t1
-  best=which.min(lapply(fit,function(x)x["val"]))
-  node=fit[[best]]
-  if(bf==5){
-    iter=d_max+1
-    return(list(tree = tree, Pmat = Pmat, iter = iter, bf = bf))
-  }
-  if(is.infinite(node["val"])){
-    bf=bf+1
-    return(list(tree = tree, Pmat = Pmat, iter = iter, bf = bf))
-  }
-
-  nodeleft=data.frame("side" = 1,"b" = node["b0"],"c0" = node["c0"],gamma = node["gamma"]
-                      , "parent" = tree$id[test$terminal[best]], "terminal" = "yes", variable = test[best,"variable"],id=nrow(tree)+1,
-                      deep=tree$deep[test$terminal[best]]+1)
-  noderight=data.frame(side = 2,  "b" = node["b1"],"c0" = node["c0"],gamma = node["gamma"]
-                       , "parent" = tree$id[test$terminal[best]], "terminal" = "yes", variable = test[best,"variable"],id=nrow(tree)+2,
-                       deep=tree$deep[test$terminal[best]]+1)
-
-
-  tree$terminal[test$terminal[best]]="no"
-
-  tree=rbind(tree,nodeleft,noderight)
-
-  p0=(1/(1+exp(-node["gamma"]*(x[,nodeleft$variable]-node["c0"]))))*Pmat[,test$terminal[best]]
-  p1=(1-(1/(1+exp(-node["gamma"]*(x[,noderight$variable]-node["c0"])))))*Pmat[,test$terminal[best]]
-  Pmat=cbind(Pmat,p0,p1)
-  Pmat[,test$terminal[best]]=NA
-  tree$b[tree$terminal=="yes"] =  node[ c(6:length(node),4,5)]
-  iter=iter+1
-  return(list(tree = tree, Pmat = Pmat, iter = iter, bf = bf))
-}
-
-grow_tree=function(x, y, p, d_max, gamma, node_obs){
-  bf=0
-
-  N=length(y)
-
-  t0 = grow0(x,y,p,N,gamma,node_obs)
-  tree = t0$tree
-  Pmat = t0$Pmat
 
   ################
   iter=1
   while(iter<=d_max){
-    titer=grow(x,y,p,N,gamma,node_obs,tree,Pmat,d_max,iter,bf)
-    tree = titer$tree
-    Pmat = titer$Pmat
-    iter = titer$iter
-    bf = titer$bf
+
+    gammai=gamma[sample(1:length(gamma),1)]
+    terminal=which(tree$terminal=="yes")
+    variables=sample(ncol(x), round(p*ncol(x)))#sort(sample(1:ncol(x), max(round(p*ncol(x)),2)))
+    test=expand.grid(variables,terminal)
+    colnames(test)=c("variable","terminal")
+
+    t1=Sys.time()
+    fit=list()
+    for(i in 1:nrow(test)){
+      xt=x[,test[i,"variable"]]
+      #xtest=stats::runif(10,min(xt)-0.1*stats::sd(xt),max(xt)+0.1*stats::sd(xt))
+      xtest=sample(xt,min(20,N),prob = Pmat[,test$terminal[i]]+0.01)
+
+      gammascale=max(stats::sd(xt),0.1)
+      middlenodes=which(is.na(colSums(Pmat)))
+      if(length(xtest)<=1){
+        fit[[i]]=c(val=Inf)
+      }else{
+        ssr=sapply(xtest,node_var_test,x=x[,test[i,"variable"]],y=y,gamma=gammai/gammascale,Pmat=Pmat,
+                   terminal=test$terminal[i],middlenodes=middlenodes,deep=tree$deep[test$terminal[i]]+1,node_obs=node_obs)
+        ssr=t(ssr)
+        ssr[is.nan(ssr)]=Inf
+        best=which.min(ssr[,1])
+        res=c(xtest[best],gammai/gammascale,ssr[best,])
+        names(res)=c("c0","gamma","val","b0","b1")
+        fit[[i]]=res
+      }
+
+
+    }
+    Sys.time()-t1
+    best=which.min(lapply(fit,function(x)x["val"]))
+    node=fit[[best]]
+    if(bf==5){
+      iter=d_max+1
+      next
+    }
+    if(is.infinite(node["val"])){
+      bf=bf+1
+      next
+    }
+
+    nodeleft=data.frame("side" = 1,"b" = node["b0"],"c0" = node["c0"],gamma = node["gamma"]
+                        , "parent" = tree$id[test$terminal[best]], "terminal" = "yes", variable = test[best,"variable"],id=nrow(tree)+1,
+                        deep=tree$deep[test$terminal[best]]+1)
+    noderight=data.frame(side = 2,  "b" = node["b1"],"c0" = node["c0"],gamma = node["gamma"]
+                         , "parent" = tree$id[test$terminal[best]], "terminal" = "yes", variable = test[best,"variable"],id=nrow(tree)+2,
+                         deep=tree$deep[test$terminal[best]]+1)
+
+
+    tree$terminal[test$terminal[best]]="no"
+
+    tree=rbind(tree,nodeleft,noderight)
+
+    p0=(1/(1+exp(-node["gamma"]*(x[,nodeleft$variable]-node["c0"]))))*Pmat[,test$terminal[best]]
+    p1=(1-(1/(1+exp(-node["gamma"]*(x[,noderight$variable]-node["c0"])))))*Pmat[,test$terminal[best]]
+    Pmat=cbind(Pmat,p0,p1)
+    Pmat[,test$terminal[best]]=NA
+    tree$b[tree$terminal=="yes"] =  node[ c(6:length(node),4,5)]
+    iter=iter+1
   }
   Pmat[is.na(Pmat)]=0
   fitted=Pmat%*%tree$b
